a094a57b91e30d0a7de2ce7a12722586c53f3d60
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-11-09 10:25:13 -0700

    Update gitpatcher to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("ca.stellardrift.gitpatcher") version "1.0.0"
+  id("ca.stellardrift.gitpatcher") version "1.1.0"
 }
 
-patches {
+gitPatcher.patchedRepos.register("loom") {
   submodule.set("fabric-loom")
   target.set(file("patched-loom"))
   patches.set(file("patches"))
