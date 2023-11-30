76654eb21bfc58012f5d6f1896fe82eb54738aa3
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-11-30 14:45:45 -0700

    Add archloom fork

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,15 @@ plugins {
   id("ca.stellardrift.gitpatcher") version "1.1.0"
 }
 
-gitPatcher.patchedRepos.register("loom") {
-  submodule.set("fabric-loom")
-  target.set(file("patched-loom"))
-  patches.set(file("patches"))
-}
-
-tasks.register("rebuildPatches") {
-  dependsOn(tasks.makePatches)
+gitPatcher.patchedRepos {
+  register("fabricLoom") {
+    submodule = "upstreams/fabric-loom"
+    target = file("quiet-fabric-loom")
+    patches = file("patches/fabric-loom")
+  }
+  register("architecturyLoom") {
+    submodule = "upstreams/architectury-loom"
+    target = file("quiet-architectury-loom")
+    patches = file("patches/architectury-loom")
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
-if (file("patched-loom").exists()) {
-  includeBuild("patched-loom")
+maybeIncludeBuild("quiet-fabric-loom")
+maybeIncludeBuild("quiet-architectury-loom")
+
+fun maybeIncludeBuild(path: String) {
+  if (file(path).exists()) {
+    includeBuild(path)
+  }
 }
 
-rootProject.name = "quiet-loom"
+rootProject.name = "quiet-loom-parent"
